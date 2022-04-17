a16a21f8cce432eb1ac821de0606b9db0e9ba4c0
dywjd12jy <dywjd3045@gmail.com>
2022-04-17 17:17:13 +0900

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 1
+const numberArray1 = [1,2,3,4,5,6,7,8,9,10];
+const numArray1Anser = numberArray1.filter(number1 => number1 % 2 === 0);
+console.log(numArray1Anser);
+
+// 2
+const numberArray2 = [1,2,3,4,5,6,7,8,9,10];
+// const numArray2Anser = numberArray2.splice((number2 => number2 >= 3));
+const numArray2Anser = numberArray2.splice(2, 8);
+console.log(numArray2Anser);
+
+
+//3
+let numberArray3 = [1,2,3,4,5,6,7,8,9,10];
+const a = "";
+// const a = "가";
+const numVal =  a === "";
+const b =["0"];
+const c = numberArray3 [ numberArray3.length -1 ];
+  numberArray3 = numVal? b:c;
+// console.log(b);
+// console.log(c);
+console.log(numberArray3);
+
+
+//4
+let numberArray4 = [1,2,3,4,5,6,7,8,9,10];
+numberArray4.map();
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <h2>
+          const numberArray = [1,2,3,4,5,6,7,8,9,10];
+        </h2>
+        <li>
+          //문제1. 짝수만 담긴 배열을 만들어보세요.
+        </li>
+        <li>
+          //문제2. 1,2가 삭제된 배열을 만들어보세요.
+        </li>
+        <li>
+          //문제3. 인수값에 빈배열을 넣으면 0을 리턴하고 빈배열이 아닌 요소를 넣으면 배열의 맨 마지막 요소를 리턴하는 함수를 만드세요.
+        </li>
+        <li>
+          //문제4. numberArray 배열을 순회해서 
+          배열요소가 
+          key이고 ㅡ 배열요소 = key
+          value는 배열원소 숫자의 영어 문자값을 갖는 객체를ㅡ value = 배열원소숫자의 영어문자값..?
+          리턴하는 함수를 만드세요.
+          <br>
+          {/* //ex) {1:'one', 2:'two'…} */}
+          </br>
+        </li>
+      </ul>
+
+
     </div>
   );
 }
